05d18eecdc612cfdd642920d35c7a96c0ce7a05f
Annie <anniebh30@gmail.com>
2017-08-18 15:33:50 +0800

    Add Touch event

--- source/javascripts/drawing.js
+++ source/javascripts/drawing.js
@@ -2,7 +2,7 @@ $().ready(function() {
   var context,
       canvasDiv = document.getElementById('canvasDiv'),
       canvas = document.createElement('canvas'),
-      canvasWidth = 800,
+      canvasWidth = 300,
       canvasHeight = 300,
       paint = false,
       clickX = [],
@@ -102,4 +102,51 @@ $().ready(function() {
        context.stroke();
     }
   }
+  // Set up touch events for mobile, etc
+  canvas.addEventListener("touchstart", function (e) {
+          mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+  canvas.addEventListener("touchend", function (e) {
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+  canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+
+  // Get the position of a touch relative to the canvas
+  function getTouchPos(canvasDom, touchEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+      x: touchEvent.touches[0].clientX - rect.left,
+      y: touchEvent.touches[0].clientY - rect.top
+    };
+  }
+  // Prevent scrolling when touching the canvas
+  document.body.addEventListener("touchstart", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+  document.body.addEventListener("touchend", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
+  document.body.addEventListener("touchmove", function (e) {
+    if (e.target == canvas) {
+      e.preventDefault();
+    }
+  }, false);
 });
